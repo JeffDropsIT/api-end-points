8ad17be656a5ceefb127e2e9f562eb58c8f996f1
Developer <mathonsiphumzile@gmail.com>
2018-07-27 08:59:16 +0200

    init commit

--- controller.js
+++ controller.js
@@ -10,7 +10,6 @@ let url = 'mongodb://localhost:27017/mydb';
 mongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
-
   db.close();
 });
 
