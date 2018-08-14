45c1cc2c8312666a4b82bf570191fd44f43268a1
Developer <mathonsiphumzile@gmail.com>
2018-08-14 16:42:32 +0200

    CRUD USERS

--- app/db/admin.js
+++ app/db/admin.js
@@ -135,9 +135,10 @@ const acceptStylistRequest = async (userId, salonObjId, status, permissions) =>{
 }
 //const user =  {fname: "Afroturf", lname: "v1", lie: "email@gmail.com", phone: "+2771008456895"};
 
-//applyAsStylist("5b72a32fc2352417f49992f8", "5b5a37b3fb6fc07c4c24d80d").then(p => console.log(p));
+applyAsStylist("5b72a32fc2352417f49992e8", "5b5a37b3fb6fc07c4c24d80d").then(p => console.log(p));
 //updateUser(updatesalons, "5b72a32fc2352417f49992f7");
 acceptStylistRequest("5b72a32fc2352417f49992f8", "5b5a37b3fb6fc07c4c24d80d", "active", ["GU"]).then(p => console.log(p))
+acceptStylistRequest("5b72a32fc2352417f49992e8", "5b5a37b3fb6fc07c4c24d80d", "active", ["null"]).then(p => console.log(p))
 // createUser( "Afroturf", "v1", "password", "email@email.com", "+2771008456895");
 // createUser( "Jeff", "v2", "jeff123", "jeff@email.com", "+277789845496");
 // createUser( "Jane", "Lane", "jane123", "jabe@email.com", "+2771008526842");
