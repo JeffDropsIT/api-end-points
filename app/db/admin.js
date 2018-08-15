dbf06f72474ac714bf9704149a47f278ef0cb3cf
Developer <mathonsiphumzile@gmail.com>
2018-08-15 14:59:50 +0200

    CRUD services and subservices

--- app/db/admin.js
+++ app/db/admin.js
@@ -6,7 +6,7 @@ let mongodbClient = mongodb.MongoClient;
 let url = 'mongodb://admin:password123@ds153841.mlab.com:53841/afroturf';
 
 
-
+const usersSchema = schema.users;
 //get database by Name
 const getDatabaseByName = async(name) =>{
  
@@ -24,7 +24,7 @@ const getDatabaseByName = async(name) =>{
   }
 };
 
-const usersSchema = schema.users;
+
 const createAnyCollection = async (dbName, collectionName, schema) =>{
     
     try{
@@ -35,8 +35,9 @@ const createAnyCollection = async (dbName, collectionName, schema) =>{
     
         }
       });
-        db.connection.close();
-    return  result;
+      db.connection.close();
+      console.log("ok: "+result.result.ok, "modified: "+ result.result.nModified);
+      return  result.result.ok, result.result.nModified;
     }catch(err){
         throw new Error(err);
     }
@@ -50,7 +51,8 @@ const insertIntoCollection = async (dbName, collectionName, user) =>{
         const db = await getDatabaseByName(dbName);
         const result = await db.db.collection(collectionName).insert(user)
         db.connection.close();
-    return  result;
+        console.log("ok: "+result.result.ok, "_id"+ result.ops[0]._id);
+        return  result.result.ok, result.ops[0]._id;
     }catch(err){
         throw new Error(err);
     }
@@ -62,7 +64,8 @@ const updateCollectionDocument = async (dbName, collectionName, data, id) =>{
         const db = await getDatabaseByName(dbName);
         const result = await db.db.collection(collectionName).update({"_id": ObjectId(id)}, {$set: data});
         db.connection.close();
-    return  result;
+        console.log("ok: "+result.result.ok, "modified: "+ result.result.nModified);
+        return  result.result.ok, result.result.nModified;
     }catch(err){
         throw new Error(err);
     }
@@ -73,45 +76,28 @@ const createUser = async (fname, lname, password, email, phone) =>{
     console.log("--createusers--");
     const user = await {fname: fname, lname: lname, password:await authentication.hashPassword(password), email: email, phone: phone};
    
-    insertIntoCollection("afroturf", "users",user).then(p => console.log("success "+p));
+    const result = await insertIntoCollection("afroturf", "users",user).then(p => console.log("success "+p));
+    db.connection.close();
+    console.log("ok: "+result.result.ok, "_id"+ result.ops[0]._id);
+    return  result.result.ok, result.ops[0]._id;
 };
-const updateUser = async (user, id) =>{
+const updateUser = async (userData, userid) =>{
 
     console.log("--updateUser--");
-    updateCollectionDocument("afroturf", "users",user,id ).then(p => console.log("success "+p));
+    const result = await updateCollectionDocument("afroturf", "users",userData,userid).then(p => console.log("success "+p));
+    db.connection.close();
+    console.log("ok: "+result.result.ok, "modified: "+ result.result.nModified);
+    return  result.result.ok, result.result.nModified;
 };
 
-
-
-const getApplicationJson =  (userId, salonObjId)=>{
-    const application = {
-        "userId": ObjectId(userId),
-        "salonObjId": ObjectId(salonObjId),
-        "status": "pending",
-        "stylistAccess": [
-            "GU"
-        ]
-    }
-    return application;
-}
-const stylistJSON =  (stylist, stylistId)=>{
-    const application = {
-        "userId": ObjectId(stylist._id),
-        "stylistId": stylistId,
-        "name": stylist.fname,
-        "gender": stylist.gender,
-        "reviews":[],
-        "rating": 1,
-    }
-    return application;
-}
 
 const getUser = async (userId)=>{
     try{
         const db = await getDatabaseByName("afroturf");
         const result = await db.db.collection("users").aggregate([{$match: {_id:ObjectId(userId)} }, {$project: {fname:1, gender:1}}]).toArray();
         db.connection.close();
-    return  JSON.parse(JSON.stringify(result));
+        console.log("ok: "+result.result.ok, "modified: "+ result.result.nModified);
+        return  JSON.parse(JSON.stringify(result));
     }catch(err){
         throw new Error(err);
     }
@@ -119,7 +105,7 @@ const getUser = async (userId)=>{
 const applyAsStylist = async (userId, salonObjId) => {
     //get currect user
     console.log("--applyAsStylist--");
-    const data = getApplicationJson(userId,salonObjId);
+    const data = schema.getApplicationJson(userId,salonObjId);
     try{
         const db = await getDatabaseByName("afroturf");
         const result = await db.db.collection("users").update({
@@ -127,11 +113,13 @@ const applyAsStylist = async (userId, salonObjId) => {
             {$addToSet: {stylistRequests:data}}, 
         );
         db.connection.close();
-    return  result.result;
+        console.log("ok: "+result.result.ok, "modified: "+ result.result.nModified);
+        return  result.result.ok, result.result.nModified;
     }catch(err){
         throw new Error(err);
     }
 }
+
 const acceptStylistRequest = async (userId, salonObjId, status, permissions) => {
     try{
         const db = await getDatabaseByName("afroturf");
@@ -141,17 +129,18 @@ const acceptStylistRequest = async (userId, salonObjId, status, permissions) => 
             {arrayFilters: [{$and: [{"stylist.salonObjId": ObjectId(salonObjId)}, {"stylist.userId": ObjectId(userId)}]}], multi : true } 
         );
         db.connection.close();
-    return  result.result;
+        console.log("ok: "+result.result.ok, "modified: "+ result.result.nModified);
+        return  result.result.ok, result.result.nModified;
     }catch(err){
         throw new Error(err);
     }
 }
 
 
-const getNextSequenceValue = async (sequenceName) => {
+const getNextSequenceValue = async (sequenceName, collectionIndex) => {
     try {
         const db = await getDatabaseByName("afroturf");
-        const sequenceDocument = await db.db.collection("stylistIndexes").findAndModify(
+        const sequenceDocument = await db.db.collection(collectionIndex).findAndModify(
             {_id: sequenceName },
             [],
             {$inc:{lastAdded:1}},
@@ -167,7 +156,7 @@ const getNextSequenceValue = async (sequenceName) => {
 
 const addStylistToSalon = async (userId, salonObjId) => {
     const stylist = await getUser(userId);
-    const stylistId = await getNextSequenceValue("stylistId");
+    const stylistId = await getNextSequenceValue("stylistId", "stylistIndexes");
 
     if(stylist == "[]"){
         console.log("NO SUCH USER "+stylist)
@@ -178,10 +167,11 @@ const addStylistToSalon = async (userId, salonObjId) => {
         const db = await getDatabaseByName("afroturf");
         const result = await db.db.collection("salons").update({
             $and:[{_id: ObjectId(salonObjId)}, {"stylists.userId": {$ne: ObjectId(stylist[0]._id)}}]},
-            {$addToSet: {stylists:stylistJSON(stylist[0], stylistId)}}
+            {$addToSet: {stylists:schema.stylistJSON(stylist[0], stylistId)}}
         );
         db.connection.close();
-    return  result.result;
+        console.log("ok: "+result.result.ok, "modified: "+ result.result.nModified);
+        return  result.result.ok, result.result.nModified;
     }catch(err){
         throw new Error(err);
     }
@@ -192,7 +182,7 @@ const addStylistToSalon = async (userId, salonObjId) => {
 //updateUser(updatesalons, "5b72a32fc2352417f49992f7");
 //acceptStylistRequest("5b72a32fc2352417f49992f8", "5b5a37b3fb6fc07c4c24d80d", "active", ["GU"]).then(p => console.log(p))
 //acceptStylistRequest("5b72a32fc2352417f49992e8", "5b5a37b3fb6fc07c4c24d80d", "active", ["null"]).then(p => console.log(p))
-addStylistToSalon("5b72a32fc2352417f49992f8", "5b5a37b3fb6fc07c4c24d80d").then(p => console.log(p))
+//addStylistToSalon("5b72a32fc2352417f49992f8", "5b5a37b3fb6fc07c4c24d80d").then(p => console.log(p))
 //getNextSequenceValue("stylistId").then(p => console.log(p))
 //getUser("5b72a32fc2352417f49992f8").then(p => console.log(p[0].fname))
 // createUser( "Afroturf", "v1", "password", "email@email.com", "+2771008456895");
@@ -204,3 +194,163 @@ addStylistToSalon("5b72a32fc2352417f49992f8", "5b5a37b3fb6fc07c4c24d80d").then(p
 
 
 //authentication.hashPassword("password").then(p => console.log(p))
+
+// CRUD  salon 
+//at least one services is required to create a salon
+const createSalon = async (userId, name, address, street, coordinates, sName, hiring) =>{
+    try {
+            //add this to users db
+        console.log("--createSalon--");
+        const salonId = await getNextSequenceValue("salonId", "salonIndexes")
+        const salon = await schema.createNewSalonForm(salonId,name, address, street, coordinates, sName);
+        let _id;
+        
+        const addedSalon = await insertIntoCollection("afroturf", "salons",salon);
+        addedSalon.result.ok == 1 ?  _id = addedSalon.ops[0]._id: _id = null;
+        //if successful
+        if(_id !== null){
+            addSalonToUserAccount(userId, _id, hiring, salonId);
+            console.log("--added to owner account-- "+_id);
+        }else{
+            console.log("--failed to add owner account--");
+            return -1
+    }
+    } catch (error) {
+        throw new Error(error);
+    }
+
+
+    
+ 
+
+}
+
+const addSalonToUserAccount = async (userId, salonObjId, hiring, salonId) => {
+    //get currect user
+    console.log("--addSalonToUserAccount--");
+    const data = schema.getActiveSalonsJsonForm(salonId,salonObjId, hiring);
+    try{
+        const db = await getDatabaseByName("afroturf");
+        const result = await db.db.collection("users").update(
+            {"_id": ObjectId(userId)},
+            {$addToSet: {salons:data}}, 
+        );
+        db.connection.close();
+        console.log(result.result.ok, result.result.nModified);
+    return  result.result.ok, result.result.nModified;
+    }catch(err){
+        throw new Error(err);
+    }
+}
+
+const updateSalon = async (salonObjId, salonData) =>{
+    //put object to update in a salon
+    try{
+        const db = await getDatabaseByName("afroturf");
+        const result = await db.db.collection("salons").update(
+            {"_id": ObjectId(salonObjId)},
+            {salonData} 
+        );
+        db.connection.close();
+        console.log(result.result.ok, result.result.nModified);
+    return  result.result.ok, result.result.nModified;
+    }catch(err){
+        throw new Error(err);
+    }
+}
+const changeAccountStatusSalon = async (salonObjId, status) => {
+    //change accountStatus to deactivated
+    try{
+        const db = await getDatabaseByName("afroturf");
+        const result = await db.db.collection("salons").update(
+            {"_id": ObjectId(salonObjId)},
+            {$set: {accountStatus: status}}, 
+        );
+        db.connection.close();
+        console.log(result.result.ok, result.result.nModified);
+    return  result.result.ok, result.result.nModified;
+    }catch(err){
+        throw new Error(err);
+    }
+
+}
+// CRUD services i.e manicure, pedicure, massages, makeup and hairstyles
+
+const changeServiceName = async (serviceName, name) =>{
+    //update service
+    try{
+        const db = await getDatabaseByName("afroturf");
+        const result = await db.db.collection("salons").update(
+            {$and: [{"_id": ObjectId(salonObjId)}, {"services._id": serviceName}]},
+            {"services.name":name, "services._id":name}
+        );
+        db.connection.close();
+        console.log("ok: "+result.result.ok, "modified: "+ result.result.nModified);
+    return  result.result.ok, result.result.nModified;
+    }catch(err){
+        throw new Error(err);
+    }
+
+}
+const deleteService = async ( serviceName) => {
+    //later use $unset
+}
+
+const addServicesToSalon = async (salonObjId, serviceName) => {
+    //check if service already exist if not proceed
+    //get currect user
+    console.log("--addServicesToSalon--");
+    const data = await schema.createNewServicesForm(serviceName);
+    try{
+        const db = await getDatabaseByName("afroturf");
+        const result = await db.db.collection("salons").update(
+            {$and: [{"_id": ObjectId(salonObjId)}, {"services._id": {$ne: serviceName}}]},
+            {$addToSet: {services:data}}, 
+        );
+        db.connection.close();
+        console.log("ok: "+result.result.ok, "modified: "+ result.result.nModified);
+    return  result.result.ok, result.result.nModified;
+    }catch(err){
+        throw new Error(err);
+    }
+}
+
+const addsubserviceToSalonServices = async (salonObjId, serviceName, type, code, price, description) => {
+    //get currect user
+    console.log("--addsubserviceToSalonServices--");
+    const data = schema.createNewSubserviceForm(type, code, price, description);
+    try{
+        const db = await getDatabaseByName("afroturf");
+        const result = await db.db.collection("salons").update(
+            {$and : [{"_id": ObjectId(salonObjId)}]},
+            {$addToSet: {"services.$[subservice].subservices":data}},
+            {arrayFilters: [{$and: [{"subservice._id":serviceName}, {"subservice.subservices.code": {$ne:data.code}}]}]}
+        );
+        db.connection.close();
+        console.log("ok: "+result.result.ok, "modified: "+ result.result.nModified);
+    return  result.result.ok, result.result.nModified;
+    }catch(err){
+        db.connection.close();
+        throw new Error(err);
+    }
+}
+
+
+// CRUD reviews salon and stylist
+
+
+const createReview = async (serviceName, serviceData) =>{
+
+}
+
+const updateReview = async (serviceName, serviceData) =>{
+
+}
+const deleteReview = async ( serviceName) => {
+
+}
+
+//createSalon("5b72a32fc2352417f49992f7", "Overflow", "Mandeni, 4491", "Thokoza Road", [-29.135888, 31.402572], "hairstyles", 1);
+addServicesToSalon("5b74167ded4ae581304ec740", "manicures");
+console.log("------")
+addsubserviceToSalonServices("5b74167ded4ae581304ec740", "manicures", "colorful", "M1F", 150, "beautiful darkgrey nails that are amazing for summer")
--- app/db/schema/schema.js
+++ app/db/schema/schema.js
@@ -1,3 +1,5 @@
+const ObjectId = require('mongodb').ObjectID;
+
 const users = {
     bsonType: "object",
     required: ["email", "password", "phone"],
@@ -50,9 +52,8 @@ const users = {
             
         },
         following: {
-            bsonType: "object",
-            properties:{
-                salons:{
+            bsonType: "array",
+            salons:{
                     bsonType: "object",
                     properties:{
                         salonIds: {
@@ -66,26 +67,24 @@ const users = {
                     }
                 }
             }
-            
-        },
+            ,
         bookings: {
-            bsonType: "object",
-            properties:{
-                bookingHistory: {
+            bsonType: "array",
+            bookingHistory: {
                     bsonType: "object",
                     properties: {
                         orders: {
-                        bsonType: "object",
-                        properties:{
-                            status: {
+                           bsonType: "object",
+                           properties:{
+                              status: {
                                 enum:["accepted", "pending", "rejected"],
                                 description: "must be one of the enum values and is not required"
                             },
-                            date: {
+                              date: {
                                 bsonType: "date",
                                 description: "must be a date and is not required"
                             },
-                            code: {
+                              code: {
                                 bsonType: "int",
                                 description: "must be an integer and is not required"
                             }
@@ -93,7 +92,6 @@ const users = {
                         
                     }
                 },
-                }
             }
             
         }
@@ -102,6 +100,252 @@ const users = {
 
 
 };
+
+
+const salons = {
+    bsonType: "object",
+    required: ["name", "location", "services", "stylists", "accountStatus"],
+    properties: {
+        name: {
+            bsonType: "string", 
+            description: "must be a string and is required"
+        },
+        
+        rating: {
+            bsonType: "int",
+            description: "must be a int and is not required"
+            },
+        created: {
+            bsonType: "date",
+            description: "must be a date and is not required"
+            },
+        accountStatus: {
+            enum: ["active", "deactivated", "blocked"],
+            description: "must be a one of the enum valuew required"
+            },
+        address: {
+            bsonType: "object",
+            properties:{
+                street: {
+                    bsonType: "string",
+                    description: "must be a string and is required"
+                },
+                address: {
+                    bsonType: "string",
+                    description: "must be a string and is required"
+                },
+                coordinates: {
+                    bsonType: "int",
+                    description: "must be an integer and is required"
+                },
+            }
+            
+        },
+        services: {
+            bsonType: "array",
+            service:{
+                bsonType: "object",
+                required: ["type", "code", "price", "description"],
+                properties:{
+                    type: {
+                         bsonType: "string",
+                            description: "must be a int and is not required"
+                        },
+                    code: {
+                        bsonType: "string",
+                        description: "must be a obj string and is not required"
+                        },
+                    price: {
+                        bsonType: "double",
+                        description: "must be a obj string and is not required"
+                    },
+                    description: {
+                        bsonType: "string",
+                        description: "must be a obj string and is not required"
+                        }
+                    }
+                }
+            
+        },
+        reviews: {
+            bsonType: "array",
+            review:{
+                bsonType: "object",
+                required: ["created", "userId", "review", "rating", "reviewId"],
+                properties:{
+                    userId: {
+                        bsonType: "object",
+                        properties: {$oid: {bsonType: "string", description:"must be a string and is required"}}
+                    },
+                    reviewId: {
+                        bsonType: "object",
+                        properties: {$oid: {bsonType: "string", description:"must be a string and is required"}}
+                    }
+                    ,
+                    created: {
+                        bsonType: "date",
+                        description: "must be a date and is required"
+                    },
+                    review: {
+                        bsonType: "string",
+                        description: "must be a obj string and is required"
+                        }
+                    },
+                    rating: {
+                        bsonType: "int",
+                        description: "must be a obj string and is required"
+                        }
+                    }
+                },
+                
+            stylists: {
+                bsonType: "array",
+                stylist:{
+                    bsonType: "object",
+                    required: ["userId", "name", "gender", "reviews", "stylistId"],
+                    properties:{
+                        userId: {
+                            bsonType: "object",
+                            properties: {$oid: {bsonType: "string", description:"must be a string and is required"}}
+                        },
+                        name: {
+                            bsonType: "string",
+                            description: "must be a obj string and is required"
+                        },
+                        stylistId: {
+                            bsonType: "int",
+                            description: "must be a obj string and is required"
+                        },
+                        gender: {
+                            bsonType: "object",
+                            description: "must be a obj string and is required"
+                        },
+                        reviews: {
+                            bsonType: "array",
+                            review:{
+                                bsonType: "object",
+                                required: ["created", "userId", "review", "rating", "reviewId"],
+                                properties:{
+                                    userId: {
+                                        bsonType: "object",
+                                        properties: {$oid: {bsonType: "string", description:"must be a string and is required"}}
+                                    },
+                                    reviewId: {
+                                        bsonType: "object",
+                                        properties: {$oid: {bsonType: "string", description:"must be a string and is required"}}
+                                    },
+                                    created: {
+                                        bsonType: "date",
+                                        description: "must be a date and is required"
+                                    },
+                                    review: {
+                                        bsonType: "string",
+                                        description: "must be a obj string and is required"
+                                        }
+                                    },
+                                    rating: {
+                                        bsonType: "int",
+                                        description: "must be a obj string and is required"
+                                        }
+                                    }
+                        },
+                        rating: {
+                            bsonType: "int",
+                            description: "must be a obj string and is required"
+                        }
+                    }
+            }
+        
+    }
+}
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//templates
+
+const getActiveSalonsJsonForm =  (genSalonVal, salonObjId, hiring)=>{
+    const application = {
+        "salonId": genSalonVal,
+        "salonObjId": ObjectId(salonObjId),
+        "role": "salonOwner",
+        "hiring": hiring
+    }
+    return application;
+}
+const getApplicationJson =  (userId, salonObjId)=>{
+    const application = {
+        "userId": ObjectId(userId),
+        "salonObjId": ObjectId(salonObjId),
+        "status": "pending",
+        "stylistAccess": [
+            "GU"
+        ]
+    }
+    return application;
+}
+const createNewSalonForm =  (genNextSalonId,name, address, street, coordinates, sName)=>{
+    const form = {
+        "name": name,
+        "salonId": genNextSalonId,
+        "rating": 1,
+        "accountStatus": "active",
+        "created": new Date(),
+        "location": {"address": address, "street": street, "coordinates":coordinates},
+        "services": [createNewServicesForm(sName)],
+        "reviews": []
+    }
+    return form;
+}
+const createNewServicesForm =  (sName)=>{
+
+    
+    const form = {
+        "name": sName,
+        "_id": sName, 
+        "created": new Date(),
+        "subservices": []
+    }
+    return form;
+}
+const createNewReviewForm =  (userId, review, description)=>{
+    
+    const form = {
+        "userId": userId,
+        "created": new Date(), //unique
+        "review": review,
+        "rating": description
+        
+    }
+    return form;
+}
+const createNewSubserviceForm =  (type, code, price, description)=>{
+    
+    const form = {
+        "type": type,
+        "code": code, //unique
+        "price": price,
+        "created": new Date(),
+        "description": description
+        
+    }
+    return form;
+}
 const stylist =  (stylist, stylistId)=>{
     const application = {
         "userId": ObjectId(stylist._id),
@@ -109,10 +353,12 @@ const stylist =  (stylist, stylistId)=>{
         "gender": stylist.gender,
         "stylistId": ObjectId(stylistId),
         "reviews":[],
-        "rating": 1,
+        "created": new Date(),
+        "rating": 1
     }
     return application;
 }
+
 
 const modifyRequestJson =  (userId, salonObjId, status, permissions)=>{
     const application = {
@@ -123,8 +369,33 @@ const modifyRequestJson =  (userId, salonObjId, status, permissions)=>{
     }
     return application;
 }
+
+const stylistJSON =  (stylist, stylistId)=>{
+    const application = {
+        "userId": ObjectId(stylist._id),
+        "stylistId": stylistId,
+        "name": stylist.fname,
+        "gender": stylist.gender,
+        "reviews":[],
+        "rating": 1,
+    }
+    return application;
+}
+
+
+
+
+
 module.exports = {
     users,
+    salons,
     stylist,
+    stylistJSON,
     modifyRequestJson,
+    createNewSalonForm,
+    createNewServicesForm,
+    createNewReviewForm,
+    createNewSubserviceForm, getApplicationJson,
+    getActiveSalonsJsonForm
+
 }
