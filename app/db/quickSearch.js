dafb06e94c64b98bbc85112ceb4cdeb4305f1341
Developer <mathonsiphumzile@gmail.com>
2018-09-10 15:30:38 +0200

    search done

--- app/db/quickSearch.js
+++ app/db/quickSearch.js
@@ -5,12 +5,6 @@ const empty = require("is-empty");
 const generalQuickSearch = async (ctx) => {
     console.log(ctx.query.filters)
 
-    contains = ctx.query.contains;
-    const filter = JSON.parse(ctx.query.filters);
-    const salon = filter.salon;
-    console.log("salon: "+salon)
-    const services = filter.services;
-    const stylist = filter.stylist;
     const location =  ctx.query.location;
     let limit = ctx.query.limit; 
     let radius = ctx.query.radius;
@@ -20,58 +14,72 @@ const generalQuickSearch = async (ctx) => {
     }
    userLocation = await task.toLocationObject(location);
 
+    contains = ctx.query.contains;
+   
+
 
 
 
 
     try {
-
-        if(!empty(salon)&& !empty(services) & !empty(stylist)){
-            
-            let salonJson = await containsOps.containsInSalons(contains, userLocation, radius, limit, salon);
-            let stylistJson = await containsOps.containsInStylist(contains, userLocation, radius, limit, stylist);
-            let serviceJson = await containsOps.containsInServices(contains, userLocation, radius, limit, services);
-            
-            const res = {res: 200, message: "successfully performed operation",
-            data: [salonJson, stylistJson, serviceJson]}
-            ctx.body = res
-        }else if(!empty(salon) && !empty(services) && empty(stylist)){
-            let salonJson = await containsOps.containsInSalons(contains, userLocation, radius, limit, salon);
-            let serviceJson = await containsOps.containsInServices(contains, userLocation, radius, limit, services);
-            const res = {res: 200, message: "successfully performed operation",
-            data: [salonJson, [{stylist:null}], serviceJson]}
-            ctx.body = res
-        }else if(!empty(salon) && !empty(stylist) && empty(services)){
-            let salonJson = await containsOps.containsInSalons(contains, userLocation, radius, limit, salon);
-            let stylistJson = await containsOps.containsInStylist(contains, userLocation, radius, limit, stylist);
-            const res = {res: 200, message: "successfully performed operation",
-            data: [salonJson, stylistJson, [{services:null}]]}
-            ctx.body = res
-        }else if(!empty(services) && !empty(stylist) && empty(salon)){
-            let serviceJson = await containsOps.containsInServices(contains, userLocation, radius, limit, services);
-            let stylistJson = await containsOps.containsInStylist(contains, userLocation, radius, limit, stylist);
-            const res = {res: 200, message: "successfully performed operation",
-            data: [[{salon:null}], stylistJson, serviceJson]}
-            ctx.body = res
-        }else if(!empty(services) && empty(stylist) && empty(salon)){
-            let serviceJson = await containsOps.containsInServices(contains, userLocation, radius, limit, services);
-            const res = {res: 200, message: "successfully performed operation",
-            data: [[{salon:null}], [{stylist:null}], serviceJson]}
-            ctx.body = res
-        }else if(!empty(stylist) && empty(services) && empty(salon)){
-            let stylistJson = await containsOps.containsInStylist(contains, userLocation, radius, limit, stylist);
-            const res = {res: 200, message: "successfully performed operation",
-            data: [[{salon:null}], stylistJson, [{services:null}]]}
-            ctx.body = res
-        }else if(!empty(salon) && empty(services) && empty(stylist)){
-            let salonJson = await containsOps.containsInSalons(contains, userLocation, radius, limit, salon);
-            const res = {res: 200, message: "successfully performed operation",
-            data: [salonJson, [{stylist:null}], [{services:null}]]}
-            ctx.body = res
+        if(ctx.query.filters !== undefined){
+            const filter = JSON.parse(ctx.query.filters);
+            const salon = filter.salon;
+            console.log("salon: "+salon)
+            const services = filter.services;
+            const stylist = filter.stylist;
+            if(!empty(salon)&& !empty(services) & !empty(stylist)){
+                
+                let salonJson = await containsOps.containsInSalons(contains, userLocation, radius, limit, salon);
+                let stylistJson = await containsOps.containsInStylist(contains, userLocation, radius, limit, stylist);
+                let serviceJson = await containsOps.containsInServices(contains, userLocation, radius, limit, services);
+                
+                const res = {res: 200, message: "successfully performed operation",
+                data: [salonJson, stylistJson, serviceJson]}
+                ctx.body = res
+            }else if(!empty(salon) && !empty(services) && empty(stylist)){
+                let salonJson = await containsOps.containsInSalons(contains, userLocation, radius, limit, salon);
+                let serviceJson = await containsOps.containsInServices(contains, userLocation, radius, limit, services);
+                const res = {res: 200, message: "successfully performed operation",
+                data: [salonJson, [{stylist:null}], serviceJson]}
+                ctx.body = res
+            }else if(!empty(salon) && !empty(stylist) && empty(services)){
+                let salonJson = await containsOps.containsInSalons(contains, userLocation, radius, limit, salon);
+                let stylistJson = await containsOps.containsInStylist(contains, userLocation, radius, limit, stylist);
+                const res = {res: 200, message: "successfully performed operation",
+                data: [salonJson, stylistJson, [{services:null}]]}
+                ctx.body = res
+            }else if(!empty(services) && !empty(stylist) && empty(salon)){
+                let serviceJson = await containsOps.containsInServices(contains, userLocation, radius, limit, services);
+                let stylistJson = await containsOps.containsInStylist(contains, userLocation, radius, limit, stylist);
+                const res = {res: 200, message: "successfully performed operation",
+                data: [[{salon:null}], stylistJson, serviceJson]}
+                ctx.body = res
+            }else if(!empty(services) && empty(stylist) && empty(salon)){
+                let serviceJson = await containsOps.containsInServices(contains, userLocation, radius, limit, services);
+                const res = {res: 200, message: "successfully performed operation",
+                data: [[{salon:null}], [{stylist:null}], serviceJson]}
+                ctx.body = res
+            }else if(!empty(stylist) && empty(services) && empty(salon)){
+                let stylistJson = await containsOps.containsInStylist(contains, userLocation, radius, limit, stylist);
+                const res = {res: 200, message: "successfully performed operation",
+                data: [[{salon:null}], stylistJson, [{services:null}]]}
+                ctx.body = res
+            }else if(!empty(salon) && empty(services) && empty(stylist)){
+                let salonJson = await containsOps.containsInSalons(contains, userLocation, radius, limit, salon);
+                const res = {res: 200, message: "successfully performed operation",
+                data: [salonJson, [{stylist:null}], [{services:null}]]}
+                ctx.body = res
+            }
         }else{
-            const res = {res: 404, message: "failed to perform operation, no filter",
-            data: [salonJson, [{stylist:null}], [{services:null}]]}
-            ctx.body = res
+            let salon = {rating:[0,5]}, stylist={rating:[0,5], gender:"male|female"}, services = {service:"", price:[0,10000]}
+            let salonJson = await containsOps.containsInSalons(contains, userLocation, radius, limit, salon);
+            let stylistJson = await containsOps.containsInStylist(contains, userLocation, radius, limit, stylist);
+            let serviceJson = await containsOps.containsInServices(contains, userLocation, radius, limit, services);
+
+            const res2 = {res: 200, message: "successfully performed operation, no filter",
+            data: [salonJson, stylistJson, serviceJson]}
+            ctx.body = res2
         }
     } catch (error) {
         console.log("failed at quickSearch")
