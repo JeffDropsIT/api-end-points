be31bbd5a369d5fa3a79bda00ba83ecf3b7a852e
Developer <mathonsiphumzile@gmail.com>
2018-09-12 15:44:19 +0200

    upload

--- app/db/stylist-operations.js
+++ app/db/stylist-operations.js
@@ -1,7 +1,4 @@
-const auth = require("./authentication");
-const awsHandler = require('../../aws/aws-handler');
 const generic = require("./generic");
-const counters = require("./counters");
 const schema = require("./schema/schema");
 const ObjectId = require('mongodb').ObjectID;
 const METERS_TO_KM = 1000;
